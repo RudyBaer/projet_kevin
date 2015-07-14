4e02481aee125897992803ed58ea53957bf824f8
youenchene <youen.chene@gadz.org>
2015-07-14 15:10:37 +0200

    More interactions

--- app.js
+++ app.js
@@ -22,17 +22,34 @@ app.get('/api/joke',function(req, res){
     });
 });
 
-
 app.post('/api/joke',function(req, res){
     var db = req.db;
 
     // Set our collection
     var collection = db.get('jokes');
 
-    console.log(req.body);
 
     // Submit to the DB
     collection.insert(req.body, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.json(doc);
+        }
+    });
+});
+
+app.put('/api/joke',function(req, res){
+    var db = req.db;
+
+    // Set our collection
+    var collection = db.get('jokes');
+
+    // Submit to the DB
+    collection.updateById(req.body._id,req.body, function (err, doc) {
         if (err) {
             // If it failed, return error
             res.send("There was a problem adding the information to the database.");
