a9043699c88bf1a478adad49af1ffd08dd686e03
youenchene <youen.chene@gadz.org>
2015-07-14 15:30:33 +0200

    Ordre de la liste

--- html/index.js
+++ html/index.js
@@ -1,17 +1,20 @@
 var app=angular.module("app", []);
 
 app.controller("main",['$scope','$http', function ($scope,$http)
-  {
+{
     $scope.kevin="Kevin";
 
     $scope.jokes=[];
 
+    $scope.predicate='';
+    $scope.reverse=false;
+
     $http.get('api/joke')
         .success(function(data) {
-          $scope.jokes=data;
+            $scope.jokes=data;
         }).
         error(function(data, status, headers, config) {
-          console.log(data);
+            console.log(data);
         });
 
     $scope.jokefilter={}
@@ -19,50 +22,55 @@ app.controller("main",['$scope','$http', function ($scope,$http)
     $scope.jokefilter.txt="";
 
     $scope.addJoke=function(joke) {
-      var j={};
-      j.txt=joke;
-      j.date=new Date();
-      $http.post('api/joke',j)
-          .success(function(data) {
-            $scope.jokes.push(j);
-            $scope.joke="";
-          }).
-          error(function(data, status, headers, config) {
-            console.log(data);
-          });
+        var j={};
+        j.txt=joke;
+        j.date=new Date();
+        $http.post('api/joke',j)
+            .success(function(data) {
+                $scope.jokes.push(j);
+                $scope.joke="";
+            }).
+            error(function(data, status, headers, config) {
+                console.log(data);
+            });
+    }
+
+    $scope.order=function(order) {
+        $scope.predicate=order;
+        $scope.reverse=!$scope.reverse;
     }
 
 
-  }]);
+}]);
 
 
-  app.controller("jokeController",['$scope','$http', function ($scope,$http)
-    {
-      $scope.addFavorite=function(joke) {
+app.controller("jokeController",['$scope','$http', function ($scope,$http)
+{
+    $scope.addFavorite=function(joke) {
         joke.favorite=true;
-          updateJoke(joke);
-      }
+        updateJoke(joke);
+    }
 
-      $scope.removeFavorite=function(joke) {
+    $scope.removeFavorite=function(joke) {
         joke.favorite=false;
-          updateJoke(joke);
-      }
+        updateJoke(joke);
+    }
 
-        $scope.plusOne=function(joke) {
-            if (joke.score == undefined) {
-                joke.score=0;
-            }
-            joke.score+=1;
-            updateJoke(joke);
+    $scope.plusOne=function(joke) {
+        if (joke.score == undefined) {
+            joke.score=0;
         }
+        joke.score+=1;
+        updateJoke(joke);
+    }
 
-        var updateJoke=function(joke) {
-            $http.put('api/joke',joke)
-                .success(function(data) {
+    var updateJoke=function(joke) {
+        $http.put('api/joke',joke)
+            .success(function(data) {
 
-                }).
-                error(function(data, status, headers, config) {
-                    console.log(data);
-                });
-        }
-    }]);
+            }).
+            error(function(data, status, headers, config) {
+                console.log(data);
+            });
+    }
+}]);
