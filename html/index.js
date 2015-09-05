adeaa7153ea7e7d476a330c3646464b780f51302
RudyBaer <baer.rudy@gmail.com>
2015-09-05 17:17:52 +0200

    step-03

--- html/index.js
+++ html/index.js
@@ -2,5 +2,10 @@ var app = angular.module("app", []);
 
 app.controller("main", function ($scope) {
         $scope.name = "Kevin";
+
+        $scope.addJoke = function (joke) {
+            console.log(joke);
+            $scope.joke = "";
+        }
     }
 );
