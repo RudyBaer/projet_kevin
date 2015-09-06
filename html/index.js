8fb44ea4b6102ef005fe3f95659996c3c3caca2d
RudyBaer <baer.rudy@gmail.com>
2015-09-06 09:59:52 +0200

    ng-repeat

--- html/index.js
+++ html/index.js
@@ -3,8 +3,15 @@ var app = angular.module("app", []);
 app.controller("main", function ($scope) {
         $scope.name = "Kevin";
 
+        $scope.jokes = [];
+
         $scope.addJoke = function (joke) {
             console.log(joke);
+            $scope.joke = "";
+            var j = {};
+            j.txt = joke;
+            j.date = new Date();
+            $scope.jokes.push(j);
             $scope.joke = "";
         }
     }
