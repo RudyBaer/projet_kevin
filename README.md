086c0f48e86d9f9ce81aa977f072323a2a16ac6b
RudyBaer <baer.rudy@gmail.com>
2015-09-06 11:40:56 +0200

    add server

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 
 `sudo apt-get install nodejs`
 
+`sudo apt-get install gcc make build-essential`
 # install bower
 
+# install mongodb
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
---
+++ app.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var app = express();
+
+
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/projetk');
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
+
+app.use(function (req, res, next) {
+    req.db = db;
+    next();
+});
+
+app.get('/api/joke', function (req, res) {
+    var db = req.db;
+    var collection = db.get('jokes');
+    collection.find({}, {}, function (e, docs) {
+        res.json(docs);
+    });
+});
+
+
+app.post('/api/joke', function (req, res) {
+    var db = req.db;
+
+
+    // Set our collection
+    var collection = db.get('jokes');
+
+    console.log(req.body);
+
+    // Submit to the DB
+    collection.insert(req.body, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.json(doc);
+        }
+    });
+});
+
+
+app.use('/', express.static(__dirname + '/html/'));
+
+
+var router = express.Router();
+
+app.use('/api', router);
+
+app.listen(3000);
