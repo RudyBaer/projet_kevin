971309bba12674b5c406d131b301a934157a9ced
RudyBaer <baer.rudy@gmail.com>
2015-10-19 11:47:32 +0200

    grunt unit test

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-karma');
+
+    grunt.initConfig({
+
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js'
+            }
+        }
+    });
+
+
+    grunt.registerTask('default', ['karma']);
+
+    grunt.registerTask('test', ['karma']);
+}
