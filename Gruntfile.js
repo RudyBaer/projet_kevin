57ecd4b611f0f0c3e67ad0664df28e3629948d1f
RudyBaer <baer.rudy@gmail.com>
2015-10-19 15:35:15 +0200

    grunt ajout concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,28 @@
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-karma');
 
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+
     grunt.initConfig({
 
         karma: {
             unit: {
                 configFile: 'karma.conf.js'
             }
-        }
+        },
+        concat: {
+            js: {
+                src: ['html/index.js', 'html/service.js', 'html/controller.js', 'html/directive/**/*.js'],
+                dest: 'html/all.js',
+            }
+        },
     });
 
 
-    grunt.registerTask('default', ['karma']);
+    grunt.registerTask('default', ['test']);
 
     grunt.registerTask('test', ['karma']);
+
+    grunt.registerTask('build', ['concat:js']);
 }
